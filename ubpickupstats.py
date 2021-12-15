235f8b669768806fa5813d813c4143e729efa134
gu.wk <gu.wenkai@sfit.shfe.com.cn>
2021-12-15 17:17:51 +0800

    change title

--- ubpickupstats.py
+++ ubpickupstats.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC demo from Streamlit')
+st.title('Uber pickups in NYC. Demo from Streamlit')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
